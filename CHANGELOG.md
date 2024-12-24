6150abf8c48780264844a4064eabdee010c6e897
mahRashidy82 <mmohsenelrashidy@gmail.com>
2024-12-24 13:38:24 +0200

    commit message

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+## 1.1.0
+
+**Bug Fixes & Enhancements**
+* Title Color not working [#25](https://github.com/belovance/QuickAlert/issues/25)
+* Add option to dismiss alert with keyboard "Enter/Cancel" pressed [#27](https://github.com/belovance/QuickAlert/issues/27)
+* Cancel button with QuickAlertType.loading [#31](https://github.com/belovance/QuickAlert/issues/31)
+
+## 1.0.2
+
+* Addition of more options to provide more control over the dialog
+* Documentation Updated
+
+## 1.0.1
+
+* ReadME updated
+* Documentation Typos Corrected
+
+## 1.0.0
+
+* Requested Updates
+* Background Color Added
+* Title Color Added
+* Text Color Added
+* Barrier Color Added
+
+## 0.0.2
+
+* ReadME updated
+* Documentation Comments Added
+
+## 0.0.1
+
+* Initial Release
+* Display animated alert dialogs such as success, error, warning, confirm, loading or even a custom dialog.
---
+++ lib/widgets/quickalert_buttons.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:quickalert/models/quickalert_options.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+
+class QuickAlertButtons extends StatelessWidget {
+  final QuickAlertOptions options;
+
+  const QuickAlertButtons({
+    Key? key,
+    required this.options,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          cancelBtn(context),
+          SizedBox(
+            width: 10,
+          ),
+          options.type != QuickAlertType.loading
+              ? okayBtn(context)
+              : const SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+
+  Widget okayBtn(context) {
+    if (!options.showConfirmBtn!) {
+      return const SizedBox();
+    }
+    final showCancelBtn =
+        options.type == QuickAlertType.confirm ? true : options.showCancelBtn!;
+
+    final okayBtn = buildButton(
+        context: context,
+        isOkayBtn: true,
+        text: options.confirmBtnText!,
+        onTap: () {
+          options.timer?.cancel();
+          options.onConfirmBtnTap != null
+              ? options.onConfirmBtnTap!()
+              : Navigator.pop(context);
+        });
+
+    if (showCancelBtn) {
+      return Expanded(child: okayBtn);
+    } else {
+      return okayBtn;
+    }
+  }
+
+  Widget cancelBtn(context) {
+    final showCancelBtn =
+        options.type == QuickAlertType.confirm ? true : options.showCancelBtn!;
+
+    final cancelBtn = buildButton(
+        context: context,
+        isOkayBtn: false,
+        text: options.cancelBtnText!,
+        onTap: () {
+          options.timer?.cancel();
+          options.onCancelBtnTap != null
+              ? options.onCancelBtnTap!()
+              : Navigator.pop(context);
+        });
+
+    if (showCancelBtn) {
+      return Expanded(child: cancelBtn);
+    } else {
+      return const SizedBox();
+    }
+  }
+
+  Widget buildButton({
+    BuildContext? context,
+    required bool isOkayBtn,
+    required String text,
+    VoidCallback? onTap,
+  }) {
+    final btnText = Text(
+      text,
+      style: defaultTextStyle(isOkayBtn),
+    );
+
+    final okayBtn = MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: options.confirmBtnColor ?? Theme.of(context!).primaryColor,
+      onPressed: onTap,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(7.5),
+          child: btnText,
+        ),
+      ),
+    );
+
+    final cancelBtn = MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: options.cancelBtnColor ?? Theme.of(context!).primaryColor,
+      onPressed: onTap,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(7.5),
+          child: btnText,
+        ),
+      ),
+    );
+
+    return isOkayBtn ? okayBtn : cancelBtn;
+  }
+
+  TextStyle defaultTextStyle(bool isOkayBtn) {
+    final textStyle = TextStyle(
+      color: isOkayBtn ? Colors.white : Colors.grey,
+      fontWeight: FontWeight.w600,
+      fontSize: 18.0,
+    );
+
+    if (isOkayBtn) {
+      return options.confirmBtnTextStyle ?? textStyle;
+    } else {
+      return options.cancelBtnTextStyle ?? textStyle;
+    }
+  }
+}
